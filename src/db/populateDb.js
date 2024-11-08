b557c2f5ff29ea486afa6d66d26d851bba0b0832
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-11-08 20:51:42 +0100

    Added SQL Query to create all the Tables

--- src/db/populateDb.js
+++ src/db/populateDb.js
@@ -3,7 +3,57 @@ import process from "process";
 
 const Client = pg.Client;
 
-const SQL = ``;
+const SQL = `
+CREATE TABLE IF NOT EXISTS game (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    game_name text
+);
+
+CREATE TABLE IF NOT EXISTS genre (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    game_genre text
+);
+
+CREATE TABLE IF NOT EXISTS developer (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    developer_name text
+);
+
+CREATE TABLE IF NOT EXISTS publisher (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    publisher_name text
+);
+
+CREATE TABLE IF NOT EXISTS game_genre (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    game_id INTEGER FOREIGN KEY,
+    genre_id INTEGER FOREIGN KEY
+);
+
+CREATE TABLE IF NOT EXISTS game_developer (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    game_id INTEGER FOREIGN KEY,
+    developer_id INTEGER FOREIGN KEY
+);
+
+CREATE TABLE IF NOT EXISTS game_publisher (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    game_id INTEGER FOREIGN KEY,
+    publisher_id INTEGER FOREIGN KEY
+);
+
+CREATE TABLE IF NOT EXISTS game_cost (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    game_id INTEGER FOREIGN KEY,
+    cost REAL
+);
+
+CREATE TABLE IF NOT EXISTS game_rating (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    game_id INTEGER FOREIGN KEY,
+    rating REAL
+);
+`;
 
 async function main() {
     const client = new Client({
