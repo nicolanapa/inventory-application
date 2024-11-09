be310bc8e6c08471fdab7a0ca3f8be2723b71689
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-11-09 16:41:13 +0100

    Added game, genre, developer and publisher Routers

---
+++ src/routes/developer.js
@@ -0,0 +1,5 @@
+import { Router } from "express";
+
+const developerRouter = Router();
+
+export default developerRouter;
---
+++ src/routes/genre.js
@@ -0,0 +1,5 @@
+import { Router } from "express";
+
+const genreRouter = Router();
+
+export default genreRouter;
