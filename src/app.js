9cc6fb9bb43f37de16cfb10162f6931d7f1eb965
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-11-09 16:42:06 +0100

    Imported all Routers

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ import express from "express";
 import process from "process";
 import url from "url";
 import path from "path";
+import gameRouter from "./routes/game";
+import genreRouter from "./routes/genre";
+import developerRouter from "./routes/developer";
+import publisherRouter from "./routes/publisher";
 
 const PORT = process.env.PORT;
 const __filename = url.fileURLToPath(import.meta.url);
@@ -12,5 +16,15 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {});
+
+app.use("/game", gameRouter);
+
+app.use("/genre", genreRouter);
+
+app.use("/developer", developerRouter);
+
+app.use("/publisher", publisherRouter);
 
 app.listen(PORT);
