7da97296b6f7f0d4378b0251877bd84d8bd79a00
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-11-10 11:08:05 +0100

    Finished the remaining SQL Query

--- src/db/defaultValues.js
+++ src/db/defaultValues.js
@@ -13,27 +13,27 @@ INSERT INTO genre (game_genre)
 VALUES ('Action'), ('Horror'), ('Adventure'), ('Novel'), ('Simulator');
 
 INSERT INTO developer (developer_name)
-VALUES (''), (''), (''), (''), ('');
+VALUES ('DVD Project'), ('Good Dogs'), ('People's Games'), ('World of Many'), ('Games');
 
-/*
 INSERT INTO publisher (developer_name)
-VALUES (''), (''), (''), (''), ('');
+VALUES ('Corporation X'), ('AllGames'), ('Front-endo'), ('Software'), ('Epic Warnings');
 
-INSERT INTO game_genre (developer_name)
-VALUES (''), (''), (''), (''), ('');
+INSERT INTO game_genre (game_id, genre_id)
+VALUES (1, 3), (5, 5), (1, 1), (2, 4), (3, 1), (3, 2), (3, 3), (4, 4), (6, 2), (7, 3), (8, 5), (9, 4), (10, 5);
 
-INSERT INTO game_developer (developer_name)
-VALUES (''), (''), (''), (''), ('');
+INSERT INTO game_developer (game_id, developer_id)
+VALUES (1, 3), (5, 5), (2, 4), (3, 1), (3, 3), (4, 4), (6, 2), (7, 3), (8, 5), (9, 4), (10, 5);
+/*
+INSERT INTO game_publisher (game_id, publisher_id)
+VALUES (1, 3), (5, 5), (2, 4), (3, 1), (4, 4), (6, 2), (7, 3), (8, 5), (9, 4), (10, 5);
 
-INSERT INTO game_publisher (developer_name)
-VALUES (''), (''), (''), (''), ('');
+INSERT INTO game_cost (game_id, cost)
+VALUES (1, 30.99), (2, 55.99), (3, 4.99), (4, 0.99), (5, 79.99), (10, 119.99), (6, 30), (8, 20), (9, 10), (7, 5.00);
 
-INSERT INTO game_cost (developer_name)
-VALUES (''), (''), (''), (''), ('');
-
-INSERT INTO game_rating (developer_name)
-VALUES (''), (''), (''), (''), ('');
-*/
+INSERT INTO game_rating (game_id, rating)
+VALUES (1, 3), (5, 5), (1, 1), (2, 4), (3, 1), (3, 2), (3, 3), (4, 4), (6, 2), (7, 3), (8, 5), (9, 4), (10, 5);
+INSERT INTO game_rating (game_id, rating)
+VALUES (1, 0.5), (5, 2), (1, 1), (2, 5), (3, 2), (3, 2.5), (3, 5), (4, 4.5), (6, 3.5), (7, 4), (8, 5), (9, 5), (10, 4.5);
 `;
 
 async function main() {
