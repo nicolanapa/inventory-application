045e04c2dcf9f87e8044775072b4919e4782b044
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-11-10 21:27:25 +0100

    Added all remaining get functions for all the Tables

--- src/db/getQueries.js
+++ src/db/getQueries.js
@@ -24,4 +24,80 @@ async function getElement(elementToQueryFrom, conditionName, elementToQuery) {
     return rows;
 }
 
-export { getElements, getElement };
+// Remove double code functions once all the Queries get tested better
+
+async function getGameGenre(gameId) {
+    const { rows } = pool.query(
+        `
+        SELECT game_genre FROM game_genre
+        INNER JOIN genre
+        ON game_genre.genre_id = genre.id
+        WHERE game_id = $1;
+        `,
+        [gameId],
+    );
+
+    return rows;
+}
+
+async function getGameDeveloper(developerId) {
+    const { rows } = pool.query(
+        `
+        SELECT developer_name FROM game_developer
+        INNER JOIN developer
+        ON game_developer.developer_id = developer.id
+        WHERE game_id = $1;
+        `,
+        [developerId],
+    );
+
+    return rows;
+}
+
+async function getGamePublisher(publisherId) {
+    const { rows } = pool.query(
+        `
+        SELECT publisher_name FROM game_publisher
+        INNER JOIN publisher
+        ON game_publisher.publisher_id = publisher.id
+        WHERE game_id = $1;
+        `,
+        [publisherId],
+    );
+
+    return rows;
+}
+
+async function getCost(gameId) {
+    const { rows } = pool.query(
+        `
+        SELECT cost FROM game_cost
+        WHERE game_id = $1;
+        `,
+        [gameId],
+    );
+
+    return rows;
+}
+
+async function getAllRatings(gameId, selectQuery = "*") {
+    const { rows } = pool.query(
+        `
+        SELECT $1 FROM 
+        WHERE game_id = $2;
+        `,
+        [selectQuery, gameId],
+    );
+
+    return rows;
+}
+
+export {
+    getElements,
+    getElement,
+    getGameGenre,
+    getGameDeveloper,
+    getGamePublisher,
+    getCost,
+    getAllRatings,
+};
