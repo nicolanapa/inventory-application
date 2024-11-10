82038eebb21ecf0d8aaadadb708598ec0b4a5b45
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-11-10 22:53:38 +0100

    Added GET / and GET /json

--- src/routes/developer.js
+++ src/routes/developer.js
@@ -1,5 +1,19 @@
 import { Router } from "express";
+import * as getQuery from "../db/queries/getQueries.js";
 
 const developerRouter = Router();
+
+developerRouter.get("/", async (req, res) => {
+    const listOfDevelopers = await getQuery.getElements("developer");
+
+    res.set({ "Content-Type": "text/html" });
+    res.status(200).render("developer", { developers: listOfDevelopers });
+});
+
+developerRouter.get("/json", async (req, res) => {
+    const listOfDevelopers = await getQuery.getElements("developer", 100);
+
+    res.status(200).json(listOfDevelopers);
+});
 
 export default developerRouter;
--- src/routes/publisher.js
+++ src/routes/publisher.js
@@ -1,5 +1,19 @@
 import { Router } from "express";
+import * as getQuery from "../db/queries/getQueries.js";
 
 const publisherRouter = Router();
+
+publisherRouter.get("/", async (req, res) => {
+    const listOfPublishers = await getQuery.getElements("publisher");
+
+    res.set({ "Content-Type": "text/html" });
+    res.status(200).render("publisher", { publishers: listOfPublishers });
+});
+
+publisherRouter.get("/json", async (req, res) => {
+    const listOfPublishers = await getQuery.getElements("publisher", 100);
+
+    res.status(200).json(listOfPublishers);
+});
 
 export default publisherRouter;
