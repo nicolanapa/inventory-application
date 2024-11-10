71e798ab5736d21be3d73ebd2af7f1d2d93fa51c
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-11-10 22:44:48 +0100

    Added GET / and GET /json

--- src/routes/genre.js
+++ src/routes/genre.js
@@ -1,5 +1,19 @@
 import { Router } from "express";
+import * as getQuery from "../db/queries/getQueries.js";
 
 const genreRouter = Router();
+
+genreRouter.get("/", async (req, res) => {
+    const listOfGenres = await getQuery.getElements("genre");
+
+    res.set({ "Content-Type": "text/html" });
+    res.status(200).render("genre", { genres: listOfGenres });
+});
+
+genreRouter.get("/json", async (req, res) => {
+    const listOfGenres = await getQuery.getElements("genre");
+
+    res.status(200).json(listOfGenres);
+});
 
 export default genreRouter;
