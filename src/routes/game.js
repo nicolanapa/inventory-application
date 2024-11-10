2aa6e9c9e9c82a1096fc48f9b3ec554af4605ebe
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-11-10 22:10:29 +0100

    Added GET / that renders 'game'

--- src/routes/game.js
+++ src/routes/game.js
@@ -1,5 +1,12 @@
 import { Router } from "express";
+import * as getQuery from "../db/getQueries.js";
 
 const gameRouter = Router();
+
+gameRouter.get("/", async (req, res) => {
+    const listOfGames = await getQuery.getElements("game");
+
+    res.render("game", { games: listOfGames });
+});
 
 export default gameRouter;
