0995ef1d275088d7ca2ba012c549f01f7f43a98e
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-11-11 20:35:57 +0100

    Added mainQuery to fetch all needed Data for GET /

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ import gameRouter from "./routes/game.js";
 import genreRouter from "./routes/genre.js";
 import developerRouter from "./routes/developer.js";
 import publisherRouter from "./routes/publisher.js";
+import * as getQuery from "./db/queries/getQueries.js";
 
 const PORT = process.env.PORT;
 const __filename = url.fileURLToPath(import.meta.url);
@@ -17,9 +18,22 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+    let mainQuery = {
+        games: await getQuery.getElements("game"),
+        gamesCost: await getQuery.getElements("game_cost"),
+        genres: await getQuery.getElements("genre"),
+        developers: await getQuery.getElements("developer"),
+        publishers: await getQuery.getElements("publisher"),
+    };
+
     res.set({ "Content-Type": "text/html" });
-    res.status(200).render("index", {});
+    res.status(200).render("index", {
+        games: mainQuery.games,
+        genres: mainQuery.genres,
+        developers: mainQuery.developers,
+        publishers: mainQuery.publishers,
+    });
 });
 
 app.use("/game", gameRouter);
