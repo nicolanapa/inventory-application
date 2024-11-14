13d11d6815228c71e0b9e4b1d5ea359ee19facd4
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-11-14 17:12:27 +0100

    Added Queries to Drop all DB Tables

---
+++ src/db/dropTables.js
@@ -0,0 +1,40 @@
+import pg from "pg";
+import process from "process";
+
+const Client = pg.Client;
+
+const SQL = `
+DROP TABLE game CASCADE;
+DROP TABLE genre CASCADE;
+DROP TABLE developer CASCADE;
+DROP TABLE publisher CASCADE;
+DROP TABLE game_genre CASCADE;
+DROP TABLE game_developer CASCADE;
+DROP TABLE game_publisher CASCADE;
+DROP TABLE game_cost CASCADE;
+DROP TABLE game_rating CASCADE;
+`;
+
+async function main() {
+    const client = new Client({
+        connectionString:
+            "postgresql://" +
+            process.env.DB_USER +
+            ":" +
+            process.env.DB_PASSWORD +
+            "@" +
+            process.env.DB_HOST +
+            ":" +
+            process.env.DB_PORT +
+            "/" +
+            process.env.DB_NAME,
+    });
+
+    await client.connect();
+    await client.query(SQL);
+    await client.end();
+
+    console.log("Dropped all Tables");
+}
+
+main();
